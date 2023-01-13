cc50e1826c2d02227631173e0420253ba0742386
OlegUdmurt1991 <olegborozdov199@gmail.com>
2023-01-13 15:20:14 +0300

    исправления замечаний

--- src/HW/java/Lesson14/mypackage/cars/Car.java
+++ src/HW/java/Lesson14/mypackage/cars/Car.java
@@ -9,7 +9,7 @@ public class Car {
 //    public static String a2 = move("a2");
     //метод
     public static String move (String a) {
-        System.out.println("This is method ! " + a);
+        System.out.println("This is methodd ! " + a);
         return a;
     }
 }
